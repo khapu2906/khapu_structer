596c0437751b427f347c180c18ee4c54b8698fdd
KhaPu <daikhanh9260@gmail.com>
2020-02-28 08:36:25 +0700

    init Khapu

---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__.'/viscera/System/define.php';
+require_once __DIR__.'/viscera/System/core/Common.php';
+require_once __DIR__.'/viscera/System/guide/primary_guide.php';
+require_once __DIR__.'/viscera/System/guide/api_guide.php';
---
+++ viscera/System/define.php
@@ -0,0 +1,6 @@
+<?php
+
+define('PATH_SYSTEM',__DIR__.'/viscera/system/');
+define('PATH_REQUEST',__DIR__.'/Request/');
+define('PATH_PUBLIC',__DIR__.'/public/');
+define('PATH_UPLOAD',__DIR__.'/uploads/');
